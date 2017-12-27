d84492700688ce43f14acac4ddcfecc2df21422c
VijayalakshmiKumar02 <Vijayalakshmi.K4@cognizant.com>
2017-12-27 10:47:24 +0530

    Delete ParentTaskRepository.java

--- src/main/java/com/projectmgmt/SinglePageApp/repository/ParentTaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.projectmgmt.SinglePageApp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.projectmgmt.SinglePageApp.model.ParentTask;
-
-public interface ParentTaskRepository extends JpaRepository<ParentTask, Long> {
-
-}
