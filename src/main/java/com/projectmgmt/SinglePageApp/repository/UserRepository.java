4b086b6e5589a25b5e3eb5a8356c210999adec2d
VijayalakshmiKumar02 <Vijayalakshmi.K4@cognizant.com>
2017-12-27 10:47:48 +0530

    Delete UserRepository.java

--- src/main/java/com/projectmgmt/SinglePageApp/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.projectmgmt.SinglePageApp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.projectmgmt.SinglePageApp.model.UserDetails;
-
-public interface UserRepository extends JpaRepository<UserDetails, Long> {
-
-}
