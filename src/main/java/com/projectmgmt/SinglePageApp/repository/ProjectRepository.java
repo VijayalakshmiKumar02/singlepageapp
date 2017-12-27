dd618e8012e8e519a4f846cc9bc5142ddb5e6166
VijayalakshmiKumar02 <Vijayalakshmi.K4@cognizant.com>
2017-12-27 10:47:32 +0530

    Delete ProjectRepository.java

--- src/main/java/com/projectmgmt/SinglePageApp/repository/ProjectRepository.java
+++
@@ -1,9 +0,0 @@
-package com.projectmgmt.SinglePageApp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.projectmgmt.SinglePageApp.model.Project;
-
-public interface ProjectRepository extends JpaRepository<Project, Long> {
-
-}
